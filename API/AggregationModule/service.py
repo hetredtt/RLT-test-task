be4200ee3aeee4ede3bfae258f94c152571648fc
hetredtt <125869909+hetredtt@users.noreply.github.com>
2024-02-29 19:19:33 +0500

    Update service.py

--- API/AggregationModule/service.py
+++ API/AggregationModule/service.py
@@ -1,5 +1,6 @@
 from datebase.db import MongoDBConnection
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 async def aggregate(dt_from, dt_upto, group_type):
     print(dt_from)
@@ -30,11 +31,32 @@ async def aggregate(dt_from, dt_upto, group_type):
             }
         ]
         aggregate = db.find_documents('mycollection', pipeline)
+        
+        if group_type == 'day':
+            all_dates = [(dt_from + timedelta(days=i)).strftime("%Y-%m-%dT%H:%M:%S")  for i in range((dt_upto - dt_from).days + 1)]
+        elif group_type == 'hour':
+            all_dates = [(dt_from + timedelta(hours=i)).strftime("%Y-%m-%dT%H:%M:%S") for i in range(int((dt_upto - dt_from).total_seconds() // 3600) + 1)]
+        else:
+            all_dates = [(dt_from + relativedelta(months=i)).strftime("%Y-%m-%dT%H:%M:%S") for i in range((dt_upto.year - dt_from.year) * 12 + dt_upto.month - dt_from.month + 1)]
+
+        labels = [datetime.strptime(result["_id"], format).strftime("%Y-%m-%dT%H:%M:%S") for result in aggregate]
+        dataset = [result["total_value"] for result in aggregate]
+
+        index_dict = {value: index for index, value in enumerate(all_dates)}
+
+        new_dataset = [0] * len(all_dates)
+
+        for value, number in zip(labels, dataset):
+            index = index_dict.get(value)
+            if index is not None:
+                new_dataset[index] = number
+
         output = {
-            "dataset": [result["total_value"] for result in aggregate],
-            "labels": [result["_id"] for result in aggregate]
+            "dataset": new_dataset,
+            "labels": all_dates
         }
         db.close()
         return output
     else:
         print("Failed to connect to MongoDB!")
+
